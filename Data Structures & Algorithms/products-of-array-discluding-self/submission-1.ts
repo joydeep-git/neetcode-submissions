4ab9d4b5b16c11d859db487cfc1693ae48638c68
Joydeep Das <joydeep.accounts@tuta.io>
2026-05-11 03:15:04 +0530

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.ts
@@ -0,0 +1,22 @@
+class Solution {
+    productExceptSelf(nums: number[]): number[] {
+
+        const res = new Array(nums.length).fill(1);
+
+        let prefix = 1;
+
+        for (let i = 0; i < nums.length; i++) {
+            res[i] = prefix;
+            prefix *= nums[i];
+        }
+
+        let suffix = 1;
+
+        for (let i = nums.length - 1; i >= 0; i--) {
+            res[i] *= suffix;
+            suffix *= nums[i];
+        }
+
+        return res;
+    }
+}
